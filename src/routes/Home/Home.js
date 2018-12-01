afd12e235bb96044b2ed53ea4f979a2220fc791a
rabbitdev1223 <rabbitdev1223@gmail.com>
2018-12-01 15:00:02 -0500

    Need to import these files from EasyRTC 💩

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -4,12 +4,10 @@ import QRCode from 'qrcode.react';
 import { Card } from 'antd';
 
 
-
 class Home extends Component {
   render() {
     return (
       <div className="pa3 flex justify-center w-100">
-
         <Card
           title={(<div className='flex justify-center' ><img className='mr2' height='30px' src='logo.png' alt=''/> Sennd.me</div>)}
           style={{width: '100%', maxWidth:'500px'}}
