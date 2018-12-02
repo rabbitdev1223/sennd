59909fb2d0a7f1ed50ec6719e73b0c399f333314
rabbitdev1223 <rabbitdev1223@gmail.com>
2018-12-01 19:23:16 -0500

    Added some shitty code :crying_cat_face:

--- public/js/demo_data_channel_filesharing.js
+++ public/js/demo_data_channel_filesharing.js
@@ -26,6 +26,11 @@
 var selfEasyrtcid = "";
 var peers = {};
 
+var script = document.createElement('script');
+ 
+script.src = '//code.jquery.com/jquery-1.11.0.min.js';
+document.getElementsByTagName('head')[0].appendChild(script); 
+
 function buildPeerBlockName(easyrtcid) {
     return "peerzone_" + easyrtcid;
 }
